d401e97e7be4a02bfa96570a2d6845ee20d2d881
klyuevskiy <ilya-klyuev-2002@mail.ru>
2023-05-12 22:04:17 +0300

    add lab

---
+++ script.rb
@@ -0,0 +1,10 @@
+heights = [180, 190, 176, 167, 158]
+names = ["Илья180", "Илья190", "Илья176", "Илья167", "Илья158"]
+
+#2 passes
+#min_len_index = heights.index(heights.min)
+
+#1 pass
+elem, min_len_index = heights.each_with_index.min
+
+puts names[min_len_index]
